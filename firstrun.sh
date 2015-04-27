b9ecc0219109440f8da3bd7935c428910411d58a
aptalca <thedjinn@yahoo.com>
2015-04-26 20:01:12 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+  service zoneminder stop
+  service apache stop
+  service mysql stop
+  
   #Search for config files, if they don't exist, copy the default ones
   if [ ! -f /config/apache.conf ]; then
     cp /root/apache.conf /config/apache.conf
@@ -11,6 +15,14 @@
     chmod a+w /config/zm.conf
   fi
   
+  if [ ! -d /config/mysql/zm ]; then
+    mkdir -p /config/mysql/zm
+    cp /var/lib/mysql/zm/* /config/mysql/zm/
+    chmod -R o+rw /config/mysql/zm
+  fi
+  
+  rm -r /var/lib/mysql/zm
+  ln -s /config/mysql/zm /var/lib/mysql/zm
   ln -s /config /etc/zm
   
   #Get docker env timezone and set system timezone
