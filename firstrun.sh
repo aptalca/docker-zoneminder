fd6879cb4059760a0fc5fe296dc7a8dca2a41efb
aptalca <thedjinn@yahoo.com>
2015-05-20 20:21:56 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
+  echo "stopping zoneminder"
   service zoneminder stop
-  service apache stop
+  echo "stopping apache"
+  service apache2 stop
+  echo "stopping mysql"
   service mysql stop
   
   #Search for config files, if they don't exist, copy the default ones
   if [ ! -f /config/apache.conf ]; then
+    echo "copying apache.conf"
     cp /root/apache.conf /config/apache.conf
     chmod a+w /config/apache.conf
   fi
   
   if [ ! -f /config/zm.conf ]; then
+    echo "copying zm.conf"
     cp /root/zm.conf /config/zm.conf
     chmod a+w /config/zm.conf
   fi
   
   # Copy mysql database if it doesn't exit
-  if [ ! -d /config/mysql/zm ]; then
-    mkdir -p /config/mysql/zm
-    cp /var/lib/mysql/zm/* /config/mysql/zm/
-    chmod -R o+rw /config/mysql/zm
+  if [ ! -d /config/mysql ]; then
+    echo "moving mysql to config folder"
+    cp -p -R /var/lib/mysql /config/
+    chmod -R a+rw /config/mysql
   fi
   
   # Copy data folder if it doesn't exist
-  if [ ! -d /config/data/zoneminder ]; then
+  if [ ! -d /config/data ]; then
+    echo "moving data folder to config folder"
     mkdir /config/data
-    cp -R -p /usr/share/zoneminder /config/data
+    cp -R -p /usr/share/zoneminder /config/data/
     rm /config/data/zoneminder/images
     rm /config/data/zoneminder/events
     rm /config/data/zoneminder/temp
@@ -34,11 +40,13 @@
     mkdir /config/data/zoneminder/temp
   fi
   
+  echo "creating symbolink links"
   rm -r /usr/share/zoneminder
-  rm -r /var/lib/mysql/zm
+  rm -r /var/lib/mysql
   ln -s /config/data/zoneminder /usr/share/zoneminder
-  ln -s /config/mysql/zm /var/lib/mysql/zm
+  ln -s /config/mysql /var/lib/mysql
   ln -s /config /etc/zm
+  chown -R mysql:mysql /var/lib/mysql
   chmod -R go+rw /config
   
   #Get docker env timezone and set system timezone
@@ -46,6 +54,7 @@
   export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
   dpkg-reconfigure tzdata
   
+  echo "starting services"
   service mysql restart
   service apache2 restart
   service zoneminder restart
