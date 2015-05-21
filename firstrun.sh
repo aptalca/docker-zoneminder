2860537e1b7f8dace21bbe3672c397d27e4712fa
aptalca <thedjinn@yahoo.com>
2015-05-20 21:42:10 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
-
-  echo "stopping zoneminder"
-  service zoneminder stop
-  echo "stopping apache"
-  service apache2 stop
-  echo "stopping mysql"
-  service mysql stop
   
   #Search for config files, if they don't exist, copy the default ones
   if [ ! -f /config/apache.conf ]; then
     echo "copying apache.conf"
     cp /root/apache.conf /config/apache.conf
-    chmod a+w /config/apache.conf
   fi
   
   if [ ! -f /config/zm.conf ]; then
     echo "copying zm.conf"
     cp /root/zm.conf /config/zm.conf
-    chmod a+w /config/zm.conf
   fi
   
   # Copy mysql database if it doesn't exit
-  if [ ! -d /config/mysql ]; then
+  if [ ! -d /config/mysql/mysql ]; then
     echo "moving mysql to config folder"
+    rm -r /config/mysql
     cp -p -R /var/lib/mysql /config/
-    chmod -R a+rw /config/mysql
   fi
   
   # Copy data folder if it doesn't exist
@@ -56,7 +47,7 @@
   dpkg-reconfigure tzdata
   
   echo "starting services"
-  service mysql restart
-  service apache2 restart
-  service zoneminder restart
+  service mysql start
+  service apache2 start
+  service zoneminder start
   
