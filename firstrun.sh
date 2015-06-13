cc787e3e20e244f59f38833d5f67d329f84eefe1
aptalca <thedjinn@yahoo.com>
2015-06-13 14:04:09 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -55,8 +55,13 @@
   export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
   dpkg-reconfigure tzdata
   
+  #fix memory issue
+  umount /dev/shm
+  mount -t tmpfs -o rw,nosuid,nodev,noexec,relatime,size=${MEM:-4096M} tmpfs /dev/shm
+  
   echo "starting services"
   service mysql start
   service apache2 start
+  sleep 10s
   service zoneminder start
   
