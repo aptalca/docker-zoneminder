8a537f00bffdb36f14f6debc07e604869979c04d
aptalca <thedjinn@yahoo.com>
2016-03-10 14:30:47 -0500

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -22,13 +22,9 @@
   if [ ! -d /config/data ]; then
     echo "moving data folder to config folder"
     mkdir /config/data
-    cp -R -p /usr/share/zoneminder /config/data/
-    rm /config/data/zoneminder/images
-    rm /config/data/zoneminder/events
-    rm /config/data/zoneminder/temp
-    mkdir /config/data/zoneminder/images
-    mkdir /config/data/zoneminder/events
-    mkdir /config/data/zoneminder/temp
+    mkdir /config/data/images
+    mkdir /config/data/events
+    mkdir /config/data/temp
   else
     echo "using existing data directory"
   fi
@@ -43,13 +39,18 @@
 
   
   echo "creating symbolink links"
-  rm -r /usr/share/zoneminder
+  rm -r /usr/share/zoneminder/events
+  rm -r /usr/share/zoneminder/images
+  rm -r /usr/share/zoneminder/temp
   rm -r /var/lib/mysql
   rm -r /usr/share/perl5/ZoneMinder
-  ln -s /config/data/zoneminder /usr/share/zoneminder
+  ln -s /config/data/events /usr/share/zoneminder/events
+  ln -s /config/data/images /usr/share/zoneminder/images
+  ln -s /config/data/temp /usr/share/zoneminder/temp
   ln -s /config/mysql /var/lib/mysql
   ln -s /config/perl5/ZoneMinder /usr/share/perl5/ZoneMinder
   chown -R mysql:mysql /var/lib/mysql
+  chown -R www-data:www-data /config/data
   chmod -R go+rw /config
   
   #Get docker env timezone and set system timezone
